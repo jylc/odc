e0186c94bab1d1169bb19603239c6a7bb1c34e90
moicena <wxb1737450829@126.com>
2026-08-12 16:05:35 +0800

    perf(migrate): 为 connect_database.project_id 加索引
    connect_database 表的 project_id 列此前无索引，仅有 PK(id) 和
    UK(connection_id, database_id)。当用户加入大量项目时，DatabaseService.list
    生成的 'WHERE project_id IN (...)' 查询会全表扫描。
    
    新增 idx_connect_database_project_id 索引，将 IN 查询从全表扫描变为
    索引扫描，显著降低大项目数场景下的查询耗时。

---
+++ server/odc-migrate/src/main/resources/migrate/common/V_4_3_4_11__alter_connect_database_add_index.sql
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025 OceanBase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+-- Add index on connect_database.project_id to accelerate project-scoped database queries.
+-- Without this index, queries like "WHERE project_id IN (...)" perform full table scans,
+-- which becomes a severe bottleneck when the number of projects (and the IN list) is large.
+CREATE INDEX idx_connect_database_project_id ON connect_database (project_id);
